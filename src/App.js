5d8186455dc387597bfd09a1c3af269a0bef7241
bloodysi <buenoct2001@gmail.com>
2020-08-07 23:40:54 -0400

    finish header, working on balance

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/header/Header';
+import Balance from './components/balance/Balance';
+
 
 function App() {
   return (
     <div className="App">
       
       <Header />
+      <Balance />
+      
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import './Header.css'
+import React, { useState } from 'react';
+import './Header.css';
 
 const Header = () => {
+
+  const [navbar, setNavbar] = useState(false)
+
   return (
+    
     <header className='header'>      
       <nav className='header__nav'>
         
         <div className='logo'>
           <h2>Cash</h2>
-          <h2 className='logo-green'>me</h2>
+          <h2 className='logo-green'>Me</h2>
         </div>
 
-        <i class="fas fa-bars"></i>
+        
+        <i 
+          className={!navbar ? "fas fa-bars navbar-icon" : "far fa-times-circle navbar-icon"}
+          onClick={()=> setNavbar(!navbar)} 
+        />
+
+    	  
+        <ul className={`header__nav-list ${navbar ? 'open' : 'close'}`}>
+          <li><i className="fas fa-book"></i>Records</li>
+          <li><i className="fas fa-chart-bar"></i>Chart</li>
+          <li><i className="fas fa-cog"></i>Setting</li>
+        </ul>
         
       </nav>
     </header>
+    
   )
 };
 
